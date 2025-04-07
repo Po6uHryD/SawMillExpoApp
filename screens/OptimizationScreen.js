6e72b6cc848afbef3ceddb178c31b98145354dfa
Sergey Kiselev <aspb.pro@gmail.com>
2025-04-07 18:21:43 +0200

    Add module OptimiztionScreen

---
+++ screens/OptimizationScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function OptimizationScreen() {
+  return (
+    <View>
+      <Text>Welcome to Optimization Screen</Text>
+    </View>
+  );
+}
